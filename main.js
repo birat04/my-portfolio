e22416b0e048ee5e331297313695c4bc40c63743
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-01-25 22:24:27 +0530

    update qualification and services

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const navMenu = document.getElementById("nav_menu");
 const navToggle = document.getElementById("nav_toggle");
-const navClose = document.getElementById("nav_close");
+const navClose = document.getElementById("nav-close");
 
 if (navToggle) {
     navToggle.addEventListener("click", () => {
@@ -8,11 +8,13 @@ if (navToggle) {
     });
 }
 
+
 if (navClose) {
     navClose.addEventListener("click", () => {
         navMenu.classList.remove("show-menu");
     });
 }
+
 
 const skillsContent = document.getElementsByClassName("skills_content");
 const skillsHeader = document.querySelectorAll(".skills_header");
@@ -25,10 +27,28 @@ function toggleSkills() {
     }
 
     if (itemClass === "skills_content skills_close") {
-        this.parentNode.className = "skills_content skills_open";
+            this.parentNode.className = "skills_content skills_open";
     }
 }
 
 skillsHeader.forEach((el) => {
     el.addEventListener("click", toggleSkills);
+});
+
+const tabs = document.querySelectorAll("[data-target]");
+const tabContents = document.querySelectorAll("[data-content]");
+tabs.forEach((tab) => {
+    tab.addEventListener("click", () => {
+        const target = document.querySelector(tab.dataset.target);
+
+        tabContents.forEach((tabContent) => {
+            tabContent.classList.remove("qualification_active");
+        });
+        target.classList.add("qualification_active");
+
+        tabs.forEach((tab) => {
+            tab.classList.remove("qualification_active");
+        });
+        tab.classList.add("qualification_active");
+    });
 });

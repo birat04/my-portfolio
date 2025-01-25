82edab6ae234ee8bc9dfa08fe04480cb3be3fd3e
Birat Roka <23btrcn071@jainuniversity.ac.in>
2025-01-26 02:25:01 +0530

    update and add media to make responsive for all devices

--- main.js
+++ main.js
@@ -53,16 +53,84 @@ tabs.forEach((tab) => {
     });
 });
 
+const sections = document.querySelectorAll("section[id]");
+window.addEventListener("scroll", scrollActive);
 
-let swiper = new Swiper(".portfolio_container", {
-    cssMode: true,
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-    pagination: {
-      el: ".swiper-pagination",
-    },
-    mousewheel: true,
-    keyboard: true,
+function scrollActive() {
+    const scrollY = window.pageYOffset;
+
+    sections.forEach((section) => {
+        const sectionHeight = section.offsetHeight;
+        const sectionTop = section.offsetTop - 50;
+        const sectionId = section.getAttribute("id");
+
+        const navLink = document.querySelector(".nav_menu a[href*=" + sectionId + "]");
+        if (navLink) {
+            if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+                navLink.classList.add("active-link");
+            } else {
+                navLink.classList.remove("active-link");
+            }
+        }
+    });
+}
+
+function scrollHeader() {
+    const nav = document.getElementById("header");
+    if (this.scrollY >= 80) {
+        nav.classList.add("scroll-header");
+    } else {
+        nav.classList.remove("scroll-header");
+    }
+}
+window.addEventListener("scroll", scrollHeader);
+
+function scrollUp() {
+    const scrollUp = document.getElementById("scroll-up");
+    if (this.scrollY >= 560) {
+        scrollUp.classList.add("scroll-up");
+    } else {
+        scrollUp.classList.remove("scroll-up");
+    }
+}
+window.addEventListener("scroll", scrollUp);
+
+const themeButton = document.getElementById("theme-button");   
+const darkTheme = "dark-theme";
+const iconTheme = "uil-sun";
+
+const selectedTheme = localStorage.getItem("selected-theme");
+const selectedIcon = localStorage.getItem("selected-icon");
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? "dark" : "light";
+const getCurrentIcon = () => themeButton.classList.contains(iconTheme) ? "uil-moon" : "uil-sun";
+
+if (selectedTheme) {
+    document.body.classList[selectedTheme === "dark" ? "add" : "remove"](darkTheme);
+    themeButton.classList[selectedIcon === "uil-moon" ? "add" : "remove"](iconTheme);
+}
+
+themeButton.addEventListener("click", () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+    localStorage.setItem("selected-theme", getCurrentTheme());
+    localStorage.setItem("selected-icon", getCurrentIcon());
 });
+
+const sr = ScrollReveal({
+    origin: "top",
+    distance: "30px",
+    duration: 2000,
+    reset: true,
+});
+
+sr.reveal(`.home_data, .home_img, .about_data, .about_img, .services_content, .menu_content, .app_data, .app_img, .contact_data, .contact_button, .footer_content`, {
+    interval: 200,
+});
+
+sr.reveal(`.skills_content, .qualification_content, .qualification_data, .portfolio_content, .footer_content`, {
+    interval: 200,
+});
+
+
+
